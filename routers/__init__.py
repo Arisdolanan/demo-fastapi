16d265144f78bfbde9e5fc2b846eaf43106410a9
aris <aris@example.com>
2024-08-15 09:22:07 +0700

    clean unused code

--- routers/__init__.py
+++ routers/__init__.py
@@ -1,9 +1,5 @@
 from fastapi import FastAPI
-from fastapi import APIRouter
-
 from controllers import hello_controller, book_controller
-
-route = APIRouter()
 
 def register_routers(app: FastAPI):
     app.include_router(hello_controller.router,prefix="/api/v1", tags=["Hello"])
